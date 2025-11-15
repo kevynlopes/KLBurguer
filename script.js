f208884e2a03b22dee889dda12371f92d9a790ad
kevynlopes <kevynlopes2019@gmail.com>
2025-11-15 17:20:50 -0300

    finished project

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var navMenu = document.querySelector(".menuNav");
-var menu = document.querySelector(".menu");
+let navMenu = document.querySelector(".menuNav");
+let menu = document.querySelector(".menu");
 
 menu.addEventListener("click", () => {
   navMenu.classList.toggle("active");
