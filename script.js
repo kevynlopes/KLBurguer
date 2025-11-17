e7168d4d782f3fecfe86a82a62119e72b5e5b250
kevynlopes <kevynlopes2019@gmail.com>
2025-11-16 23:15:46 -0300

    add: toast buy

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ let menu = document.querySelector(".menu");
 menu.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
+
+function addToCart() {
+  let toastAddToCart = document.createElement("div");
+  let body = document.querySelector("body");
+  toastAddToCart.classList.add("toast");
+
+  toastAddToCart.innerHTML = "Burguer adicionado ao carrinho!";
+
+  body.appendChild(toastAddToCart);
+
+  setTimeout(() => {
+    body.removeChild(toastAddToCart);
+  }, 2000);
+}
